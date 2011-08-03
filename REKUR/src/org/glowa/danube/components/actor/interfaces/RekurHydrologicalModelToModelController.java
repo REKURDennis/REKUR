229a1cae5a80870a42ba471ea3a8516483b00e63
REKURDennis <dennisjoswig@gmx.de>
2011-08-03 17:42:42 +0200

    Actor to Model

---
+++ REKUR/src/org/glowa/danube/components/actor/interfaces/RekurHydrologicalModelToModelController.java
@@ -0,0 +1,7 @@
+package org.glowa.danube.components.actor.interfaces;
+
+import org.glowa.danube.components.DanubiaInterface;
+
+public interface RekurHydrologicalModelToModelController extends DanubiaInterface
+{
+}
