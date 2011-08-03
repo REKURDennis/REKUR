03570314d31c1b458cea124520fe24c4d2a503ae
REKURDennis <dennisjoswig@gmx.de>
2011-08-03 17:42:51 +0200

    Actor to Model

---
+++ REKUR/src/org/glowa/danube/components/actor/interfaces/RekurTouristModelToModelController.java
@@ -0,0 +1,8 @@
+package org.glowa.danube.components.actor.interfaces;
+
+import org.glowa.danube.components.DanubiaInterface;
+
+public interface RekurTouristModelToModelController extends DanubiaInterface
+{
+  public int[][][] getNumberOfTourists();
+}
