0f325fe2fdd6f6c0254f88303abca1ccf3818dee
REKURDennis <dennisjoswig@gmx.de>
2011-08-04 01:08:37 +0200

    ClimateData hierachie

---
+++ REKUR/src/org/glowa/danube/components/actor/utilities/AggregatedClimateData.java
@@ -0,0 +1,6 @@
+package org.glowa.danube.components.actor.utilities;
+
+public class AggregatedClimateData extends ClimateData{
+	public float precipitationMax = 0f;
+	public int precepitationDayCounter = 0;
+}
