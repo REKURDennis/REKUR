222d96b43c1a2b37d7510c3a160b697e5a8f935e
REKURDennis <dennisjoswig@gmx.de>
2011-08-03 17:42:31 +0200

    Actor to Model

---
+++ REKUR/src/org/glowa/danube/components/actor/interfaces/RekurDestinationModelToModelController.java
@@ -0,0 +1,17 @@
+package org.glowa.danube.components.actor.interfaces;
+
+import java.util.HashMap;
+
+import org.glowa.danube.components.DanubiaInterface;
+import org.glowa.danube.components.actor.utilities.ClimateData;
+
+public interface RekurDestinationModelToModelController extends DanubiaInterface
+{
+	//Climate related variables
+	
+	//Destination specific variables
+	public HashMap<Integer, boolean[]> getHolidayTypes();
+	public HashMap<Integer, Integer> getCountryIDs();     
+	public HashMap<Integer, ClimateData> getDailyClimateData();
+	public HashMap<Integer, ClimateData> getLastMonthClimateData();
+}
