d567f85ec78db7b9400ca01e30dcb6e9be8ca0fc
REKURDennis <dennisjoswig@gmx.de>
2011-08-03 17:52:25 +0200

    Actor to Model

---
+++ REKUR/src/org/glowa/danube/components/actor/ModelController.java
@@ -0,0 +1,136 @@
+package org.glowa.danube.components.actor;
+
+import java.util.HashMap;
+
+import org.glowa.danube.components.actor.interfaces.*;
+import org.glowa.danube.components.actor.utilities.ClimateData;
+import org.glowa.danube.simulation.controller.AbstractController;
+import org.glowa.danube.tables.FloatDataTable;
+import org.glowa.danube.tables.MassPerAreaTable;
+import org.glowa.danube.tables.TemperatureTable;
+
+public class ModelController extends AbstractController implements ModelControllerToRekurDestinationModel, 
+	ModelControllerToRekurTouristModel, ModelControllerToRekurHydrologicalModel{
+	
+	private RekurTouristModelToModelController tourist = null;
+	private RekurClimateModelToModelController climate = null;
+	private RekurDestinationModelToModelController destination = null;
+	private RekurHydrologicalModelToModelController hydrological = null;
+	
+	/**
+	 * Inits the Interfaceconnections
+	 */
+	
+	protected void init() throws RuntimeException {
+		// -- internal interfaces --------------------------------------------------
+		try {
+			tourist = (RekurTouristModelToModelController) getImport("org.glowa.danube.components.actor.interfaces.RekurTouristModelToActorController");
+		} catch (Exception ex) {
+			this.logger().warn(ex);
+		}
+		try {
+			climate = (RekurClimateModelToModelController) getImport("org.glowa.danube.components.actor.interfaces.RekurClimateModelToActorController");
+		} catch (Exception ex) {
+			this.logger().warn(ex);
+		}
+		try {
+			destination = (RekurDestinationModelToModelController) getImport("org.glowa.danube.components.actor.interfaces.RekurDestinationModelToActorController");
+		} catch (Exception ex) {
+			this.logger().warn(ex);
+		}
+		try {
+			hydrological = (RekurHydrologicalModelToModelController) getImport("org.glowa.danube.components.actor.interfaces.RekurHydrologicalModelToActorController");
+		} catch (Exception ex) {
+			this.logger().warn(ex);
+		}
+	}
+	
+	/**
+	 * Cleans up references in case of abort.
+	 */
+	protected void processAbort() {
+	}
+	
+//	Touristmodel to DestinationModel interfacemethods
+	@Override
+	public int[][][] getNumberOfTourists() {
+		// TODO Auto-generated method stub
+		return tourist.getNumberOfTourists();
+	}
+
+	
+//	ClimateModel to DestinationModel interfacemethods
+	@Override
+	public TemperatureTable getAirTemperatureDailyMean() {
+		// TODO Auto-generated method stub
+		return climate.getAirTemperatureDailyMean();
+	}
+
+	@Override
+	public TemperatureTable getAirTemperatureDailyMax() {
+		// TODO Auto-generated method stub
+		return climate.getAirTemperatureDailyMax();
+	}
+
+	@Override
+	public TemperatureTable getAirTemperatureDailyMin() {
+		// TODO Auto-generated method stub
+		return climate.getAirTemperatureDailyMin();
+	}
+
+	@Override
+	public MassPerAreaTable getPrecipitationDailySum() {
+		// TODO Auto-generated method stub
+		return climate.getPrecipitationDailySum();
+	}
+
+	@Override
+	public FloatDataTable getSunshineDurationDailySum() {
+		// TODO Auto-generated method stub
+		return climate.getSunshineDurationDailySum();
+	}
+
+	@Override
+	public FloatDataTable getWindSpeedDailyMean() {
+		// TODO Auto-generated method stub
+		return climate.getWindSpeedDailyMean();
+	}
+
+	@Override
+	public FloatDataTable getWindSpeedDailyMax() {
+		// TODO Auto-generated method stub
+		return climate.getWindSpeedDailyMax();
+	}
+
+	@Override
+	public FloatDataTable getRelativeHuminityDailyMean() {
+		// TODO Auto-generated method stub
+		return climate.getRelativeHuminityDailyMean();
+	}
+
+	
+//	Destinationmodel to TouristModel interfaceMethods
+	@Override
+	public HashMap<Integer, ClimateData> getDailyClimateData() {
+		// TODO Auto-generated method stub
+		return destination.getDailyClimateData();
+	}
+
+	@Override
+	public HashMap<Integer, ClimateData> getLastMonthClimateData() {
+		// TODO Auto-generated method stub
+		return destination.getLastMonthClimateData();
+	}
+	
+	@Override
+	public HashMap<Integer, boolean[]> getHolidayTypes() {
+		// TODO Auto-generated method stub
+		return destination.getHolidayTypes();
+	}
+
+	@Override
+	public HashMap<Integer, Integer> getCountryIDs() {
+		// TODO Auto-generated method stub
+		return destination.getCountryIDs();
+	}
+}
