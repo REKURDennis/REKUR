cb6514fb2844e87b218d309a5ad1dc251a2d3f42
REKURDennis <dennisjoswig@gmx.de>
2011-08-10 00:54:34 +0200

    ssdf

---
+++ REKUR/src/org/glowa/danube/components/actor/touristmodel/deepactors/ScenarioAction1.java
@@ -0,0 +1,11 @@
+package org.glowa.danube.components.actor.touristmodel.deepactors;
+
+import org.glowa.danube.deepactors.actors.action.AbstractAction;
+
+/**
+ * @author Dennis Joswig
+ *@see AbstractAction
+ */
+
+public class ScenarioAction1 extends AbstractAction<DA_SourceArea>{
+}
