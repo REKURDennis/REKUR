e853cf027401b98b5fff9d86865bacaea09cc754
REKURDennis <dennisjoswig@gmx.de>
2011-08-10 00:59:44 +0200

    climatedata add thi

--- REKUR/src/org/glowa/danube/components/actor/utilities/ClimateData.java
+++ REKUR/src/org/glowa/danube/components/actor/utilities/ClimateData.java
@@ -5,10 +5,10 @@ public class ClimateData {
 	public float airTemperatureMax = 0f;
 	public float airTemperatureMin = 400f;
 	public float precipitationSum = 0f;
-	public float precipitationMax = 0f;
-	public int precepitationDayCounter = 0;
 	public float sunshineDurationSum = 0f;
 	public float windSpeedMean = 0f;
 	public float windSpeedMax = 0f;
-	public float relativeHuminityMean = 0f;
+	public float relativeHumidityMean = 0f;
+	public float watertemp = 0f;
+	public int temperatureHumidityIndex = 0;
 }
