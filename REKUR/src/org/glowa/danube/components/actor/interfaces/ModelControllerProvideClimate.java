5bb713d5558c104e467f11b49c143317936edce1
REKURDennis <dennisjoswig@gmx.de>
2011-08-09 18:37:36 +0200

    Interface hierachie

---
+++ REKUR/src/org/glowa/danube/components/actor/interfaces/ModelControllerProvideClimate.java
@@ -0,0 +1,19 @@
+package org.glowa.danube.components.actor.interfaces;
+
+import org.glowa.danube.tables.FloatDataTable;
+import org.glowa.danube.tables.MassPerAreaTable;
+import org.glowa.danube.tables.TemperatureTable;
+
+public interface ModelControllerProvideClimate {
+	/*
+	 * ClimateModel to other Models
+	 */
+	public TemperatureTable getAirTemperatureDailyMean();
+	public TemperatureTable getAirTemperatureDailyMax();
+	public TemperatureTable getAirTemperatureDailyMin();
+	public MassPerAreaTable getPrecipitationDailySum();
+	public FloatDataTable getSunshineDurationDailySum();
+	public FloatDataTable getWindSpeedDailyMean();
+	public FloatDataTable getWindSpeedDailyMax();
+	public FloatDataTable getRelativeHuminityDailyMean();
+}
