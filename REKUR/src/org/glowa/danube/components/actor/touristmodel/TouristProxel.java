c4faa69a78cb084e87337043fab7e1d43f5140e2
REKURDennis <dennisjoswig@gmx.de>
2011-08-10 00:57:22 +0200

    Proxelchange

---
+++ REKUR/src/org/glowa/danube/components/actor/touristmodel/TouristProxel.java
@@ -0,0 +1,20 @@
+
+package org.glowa.danube.components.actor.touristmodel;
+
+import org.glowa.danube.simulation.model.proxel.AbstractProxel;
+
+
+/** 
+ * Is the class for one Touristproxel object with its values and methods.
+ * @author Dennis Joswig
+ * 
+ * 
+ * 
+ */
+
+public class TouristProxel extends AbstractProxel
+{
+	public static final long serialVersionUID = 1;
+}
+
+
