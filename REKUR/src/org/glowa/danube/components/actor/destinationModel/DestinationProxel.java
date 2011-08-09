89ce3c53759e4766ff60d45ff36c01ff5e43b80a
REKURDennis <dennisjoswig@gmx.de>
2011-08-10 00:48:06 +0200

    proxelchange

---
+++ REKUR/src/org/glowa/danube/components/actor/destinationModel/DestinationProxel.java
@@ -0,0 +1,24 @@
+
+package org.glowa.danube.components.actor.destinationModel;
+
+import org.glowa.danube.components.actor.utilities.ClimateData;
+import org.glowa.danube.simulation.model.proxel.AbstractProxel;
+
+
+/** 
+ * Specifies the additional attributes of the DestinationModel proxel
+ * @author Dennis Joswig
+ * 
+ */
+
+public class DestinationProxel extends AbstractProxel
+{
+	public ClimateData cd = new ClimateData();
+	
+	public static final long serialVersionUID = 1;
+	
+	
+	
+}
+
+
