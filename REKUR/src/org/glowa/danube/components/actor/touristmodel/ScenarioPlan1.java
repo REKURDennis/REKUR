b9b683e8dc990a1cecc0508d540a6b1237d227ac
REKURDennis <dennisjoswig@gmx.de>
2011-08-10 00:57:33 +0200

    plan1

---
+++ REKUR/src/org/glowa/danube/components/actor/touristmodel/ScenarioPlan1.java
@@ -0,0 +1,12 @@
+package org.glowa.danube.components.actor.touristmodel;
+
+import org.glowa.danube.deepactors.actors.plan.AbstractPlan;
+
+
+/**Plan for Scenario1 operation
+ * @author Dennis Joswig
+ *
+ */
+public class ScenarioPlan1 extends AbstractPlan {
+
+}
