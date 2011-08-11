43045b866e3ec054f9f08d1699ac4afbdbe4482a
REKURDennis <dennisjoswig@gmx.de>
2011-08-11 10:36:15 +0200

    abstractactormodelproxel new

---
+++ REKUR/src/org/glowa/danube/components/actor/utilities/AbstractActorModelProxel.java
@@ -0,0 +1,9 @@
+package org.glowa.danube.components.actor.utilities;
+
+import org.glowa.danube.simulation.model.proxel.AbstractProxel;
+
+public abstract class AbstractActorModelProxel extends AbstractProxel {
+	public ClimateData cd = new ClimateData();
+
+	public static final long serialVersionUID = 1;
+}
